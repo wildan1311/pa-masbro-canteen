862c401e33ec3e190283ee8f2d3a324867b8f507
wildan1311 <wmaaf1311@gmail.com>
2023-09-03 07:44:22 +0700

    make soft deletes

--- app/Models/MenusKelola.php
+++ app/Models/MenusKelola.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MenusKelola extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'menus_kelola';
 
--- app/Models/TransaksiDetail.php
+++ app/Models/TransaksiDetail.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TransaksiDetail extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'transaksi_detail';
     protected $fillable=[
