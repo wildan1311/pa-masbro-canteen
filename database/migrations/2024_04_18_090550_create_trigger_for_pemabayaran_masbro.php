2e8f7e9b36f01f26f6c4a414fd7864d44bc72eef
wildan1311 <wmaaf1311@gmail.com>
2024-04-20 13:48:53 +0700

    rubah trigger pembayaran masbro posgres

--- database/migrations/2024_04_18_090550_create_trigger_for_pemabayaran_masbro.php
+++ database/migrations/2024_04_18_090550_create_trigger_for_pemabayaran_masbro.php
@@ -15,14 +15,19 @@ class CreateTriggerForPemabayaranMasbro extends Migration
     public function up()
     {
         DB::unprepared('
-            CREATE OR REPLACE TRIGGER tr_Pembayaran_Masbro AFTER INSERT ON transaksi
-            FOR EACH ROW
+            CREATE OR REPLACE FUNCTION tr_Pembayaran_Masbro()
+            RETURNS TRIGGER AS $$
             BEGIN
-                IF NEW.isAntar = 1 THEN
-                    INSERT INTO pembayaran_masbro (total, created_at, updated_at)
-                    VALUES (NEW.ongkos_kirim, NOW(), NULL);
+                IF NEW."isAntar" = 1 THEN
+                    INSERT INTO pembayaran_masbro (total, created_at)
+                    VALUES (NEW."ongkos_kirim", NOW());
                 END IF;
+                RETURN NEW;
             END;
+            $$ LANGUAGE plpgsql;
+
+            CREATE OR REPLACE TRIGGER tr_Pembayaran_Masbro AFTER INSERT ON transaksi
+            FOR EACH ROW EXECUTE FUNCTION tr_Pembayaran_Masbro();
         ');
     }
 
@@ -33,6 +38,9 @@ class CreateTriggerForPemabayaranMasbro extends Migration
      */
     public function down()
     {
-        DB::unprepared('DROP TRIGGER `tr_Pembayaran_Masbro`');
+        DB::unprepared('
+        DROP TRIGGER IF EXISTS tr_Pembayaran_Masbro ON transaksi;
+        DROP FUNCTION IF EXISTS tr_Pembayaran_Masbro();
+        ');
     }
 }
