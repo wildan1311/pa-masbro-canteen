8b0ea7e08c164b5d7a1538dc915ce594b9845e8c
wildan1311 <wmaaf1311@gmail.com>
2024-04-23 06:07:37 +0700

    kembalikan migration trigger jadi mysql

--- database/migrations/2024_04_18_090550_create_trigger_for_pemabayaran_masbro.php
+++ database/migrations/2024_04_18_090550_create_trigger_for_pemabayaran_masbro.php
@@ -15,19 +15,14 @@ class CreateTriggerForPemabayaranMasbro extends Migration
     public function up()
     {
         DB::unprepared('
-            CREATE OR REPLACE FUNCTION tr_Pembayaran_Masbro()
-            RETURNS TRIGGER AS $$
-            BEGIN
-                IF NEW."isAntar" = 1 THEN
-                    INSERT INTO pembayaran_masbro (total, created_at)
-                    VALUES (NEW."ongkos_kirim", NOW());
-                END IF;
-                RETURN NEW;
-            END;
-            $$ LANGUAGE plpgsql;
-
             CREATE OR REPLACE TRIGGER tr_Pembayaran_Masbro AFTER INSERT ON transaksi
-            FOR EACH ROW EXECUTE FUNCTION tr_Pembayaran_Masbro();
+            FOR EACH ROW
+            BEGIN
+                IF NEW.isAntar = 1 THEN
+                    INSERT INTO pembayaran_masbro (total, created_at, updated_at)
+                    VALUES (NEW.ongkos_kirim, NOW(), NULL);
+                END IF;
+            END;
         ');
     }
 
@@ -38,9 +33,6 @@ class CreateTriggerForPemabayaranMasbro extends Migration
      */
     public function down()
     {
-        DB::unprepared('
-        DROP TRIGGER IF EXISTS tr_Pembayaran_Masbro ON transaksi;
-        DROP FUNCTION IF EXISTS tr_Pembayaran_Masbro();
-        ');
+        DB::unprepared('DROP TRIGGER `tr_Pembayaran_Masbro`');
     }
 }
