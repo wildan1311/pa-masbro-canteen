7d2405da3155ea8769fb0703697730f96f2ce28a
wildan1311 <wmaaf1311@gmail.com>
2024-05-02 13:46:21 +0700

    benerin harga keuangan

--- resources/views/pages/keuangan/index.blade.php
+++ resources/views/pages/keuangan/index.blade.php
@@ -98,7 +98,7 @@
                                                 {{-- <p class="text-gray-600 whitespace-no-wrap">USD</p> --}}
                                             </td>
                                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                <p class="text-gray-900 whitespace-no-wrap">Rp{{number_format($summary->jumlah_semua, 0, ',', '.')}}</p>
+                                                <p class="text-gray-900 whitespace-no-wrap">Rp{{number_format($detail->menusKelola->harga, 0, ',', '.')}}</p>
                                                 {{-- <p class="text-gray-600 whitespace-no-wrap">Due in 3 days</p> --}}
                                             </td>
                                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
