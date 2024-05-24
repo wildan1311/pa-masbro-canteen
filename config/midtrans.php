9e8ceca9f71eac13805f2101dd3ab785064d79c2
wildan1311 <wmaaf1311@gmail.com>
2024-05-24 21:21:09 +0700

    brnrtin config midtrans biar diaturnya lewat env

--- config/midtrans.php
+++ config/midtrans.php
@@ -2,10 +2,10 @@
 
 return [
     'mercant_id' => env('MIDTRANS_MERCHAT_ID'),
-    'client_key' => env('MIDTRANS_CLIENT_KEY'),
-    'server_key' => env('MIDTRANS_SERVER_KEY'),
+    'client_key' => env('MIDTRANS_IS_PRODUCTION') ? env('MIDTRANS_CLIENT_KEY_PROD') : env('MIDTRANS_CLIENT_KEY_SAND'),
+    'server_key' => env('MIDTRANS_IS_PRODUCTION') ? env('MIDTRANS_SERVER_KEY_PROD') : env('MIDTRANS_SERVER_KEY_SAND'),
 
-    'is_production' => false,
-    'is_sanitized' => false,
-    'is_3ds' => false,
+    'is_production' => env('MIDTRANS_IS_PRODUCTION'),
+    'is_sanitized' => env('MIDTRANS_IS_SANITIZED'),
+    'is_3ds' => env('MIDTRANS_IS_3DS'),
 ];
