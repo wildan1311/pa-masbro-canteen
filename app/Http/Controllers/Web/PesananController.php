fed8af46f49969a0fe9c74dd87cb41e1a8f99d1d
akbar <akbardammahum02@gmail.com>
2024-07-02 16:28:39 +0700

    berhasil membuat pagination pada pesanan

--- app/Http/Controllers/Web/PesananController.php
+++ app/Http/Controllers/Web/PesananController.php
@@ -16,18 +16,42 @@ class PesananController extends Controller
         $user = Auth::user();
 
         $tenant = Tenants::where("user_id", $user->id)->first();
-        $dataPesanan = Transaksi::with([
+
+        $allPesananNonDitolak = Transaksi::with([
             'listTransaksiDetail' => function ($query) use ($tenant) {
                 $query->whereHas('menus', function ($menus) use ($tenant) {
                     $menus->where('tenant_id', $tenant->id);
                 });
             }, 'user'
-        ])->whereHas('listTransaksiDetail.menus.tenants', function ($query) use ($tenant) {
-            $query->where('id', $tenant->id);
-        })->whereNotIn('status', ['pending', 'expire', 'cancel'])->orderByDesc('created_at')->get();
+        ])
+            ->where('transaksi.status', '!=', 'pesanan_ditolak')
+            ->whereHas('listTransaksiDetail.menus.tenants', function ($query) use ($tenant) {
+                $query->where('id', $tenant->id);
+            })
+            ->whereNotIn('status', ['pending', 'expire', 'cancel'])
+            ->orderByDesc('created_at');
+        $allPesananDitolak = Transaksi::with([
+            'listTransaksiDetail' => function ($query) use ($tenant) {
+                $query->whereHas('menus', function ($menus) use ($tenant) {
+                    $menus->where('tenant_id', $tenant->id);
+                });
+            }, 'user'
+        ])
+            ->where('transaksi.status', 'pesanan_ditolak')
+            ->whereHas('listTransaksiDetail.menus.tenants', function ($query) use ($tenant) {
+                $query->where('id', $tenant->id);
+            })
+            ->whereNotIn('status', ['pending', 'expire', 'cancel'])
+            ->orderByDesc('created_at');
 
-        // dd($dataPesanan);
+        $pesananDitolak = $allPesananDitolak->paginate($perPage = 15, $columns = ['*'], $pageName = 'pesanan_ditolak_page');
+        $pesananNonDitolak = $allPesananNonDitolak->paginate($perPage = 15, $columns = ['*'], $pageName = 'pesanan_non_ditolak_page');
 
-        return view('pages.pesanan.index', compact('dataPesanan'));
+        return view('pages.pesanan.index', compact(
+            'pesananNonDitolak',
+            'pesananDitolak',
+            // 'pesananTotal',
+            // 'pesananTotalBatal'
+        ));
     }
 }
